f5c40087b1bbbf27f09c67438d5b0a07b57b7c05
JP Richardson <jprichardson@gmail.com>
2014-04-30 14:01:12 -0500

    added test vectors and initial test for sha512

---
+++ test/sha512.test.js
@@ -0,0 +1,16 @@
+var sha512 = require('../')
+
+require('terst')
+
+var testVectors = require('./sha512-vectors.test.js')
+
+describe('sha512', function() {
+  describe('> when test vectors', function() {
+    it('should produce the correct result', function() {
+      testVectors.forEach(function(v) {
+        var out = sha512(v[0])
+        EQ (out.toString('hex'), v[1])
+      })
+    })
+  })
+})
