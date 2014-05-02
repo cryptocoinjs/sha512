fea1ff24045d6e2154ef89ce749aac223449cf1c
JP Richardson <jprichardson@gmail.com>
2014-05-01 21:02:30 -0500

    added browser compatible test

--- test/sha512.test.js
+++ test/sha512.test.js
@@ -7,8 +7,8 @@ var testVectors = require('./sha512-vectors.test.js')
 describe('sha512', function() {
   describe('> when test vectors', function() {
     it('should produce the correct result', function() {
-      testVectors.forEach(function(v) {
-        var out = sha512(v[0])
+      testVectors.forEach(function(v, i) {
+        var out = sha512(new Buffer(v[0], 'utf8'))
         EQ (out.toString(), v[1])
       })
     })
