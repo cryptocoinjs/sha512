3e1525e4099f21bf45da290d4057b28bcacee84f
JP Richardson <jprichardson@gmail.com>
2014-05-23 16:25:50 -0500

    removed custom Base class from hmac

--- test/hmac-sha512.test.js
+++ test/hmac-sha512.test.js
@@ -8,7 +8,7 @@ describe('hmac-sha512', function() {
       var inp = new Buffer("hello man")
       var key = new Buffer("super secret")
       var outp = "292f154b455f464131e8af89478e0a2af37fecf5de2e9cf998df1d9447f5856d146a1660708564bb7fd76d2fe80ab92a31af70e1d69a34f6b5b4839bdb26cbab"
-      var hasher = hmac.create(key)
+      var hasher = hmac(key)
     
       EQ (hasher.finalize(inp).toString('hex'), outp)
     })
@@ -19,7 +19,7 @@ describe('hmac-sha512', function() {
       var inp = "hello man"
       var key = "super secret"
       var outp = "292f154b455f464131e8af89478e0a2af37fecf5de2e9cf998df1d9447f5856d146a1660708564bb7fd76d2fe80ab92a31af70e1d69a34f6b5b4839bdb26cbab"
-      var hasher = hmac.create(key)
+      var hasher = hmac(key)
     
       EQ (hasher.finalize(inp).toString('hex'), outp)
     })
